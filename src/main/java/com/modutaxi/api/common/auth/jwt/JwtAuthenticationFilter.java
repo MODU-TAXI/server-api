3dea87a95f764792ee5209516dbc870ebc984bae
bora <youonl5@naver.com>
2024-03-14 20:20:55 +0900

    [MS-11] Infra: Swagger 설정

--- src/main/java/com/modutaxi/api/common/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/modutaxi/api/common/auth/jwt/JwtAuthenticationFilter.java
@@ -24,18 +24,20 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
      */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
+            throws IOException, ServletException {
         // 헤더에 Authorization이 아닌 refreshToken이 있다면 refreshToken 사용
         String refreshToken = jwtTokenProvider.resolveRefreshToken((HttpServletRequest) request);
         String token = jwtTokenProvider.resolveAccessToken((HttpServletRequest) request);
         // 헤더에 refreshToken을 보냈다면 refreshToken 유효성 검사 및 authentication 세팅
         if (refreshToken != null && ((HttpServletRequest) request).getRequestURI()
-            .equals("/refresh") && jwtTokenProvider.validateRefreshToken(refreshToken)) {
+                .equals("/refresh") && jwtTokenProvider.validateRefreshToken(refreshToken)) {
+            if (refreshToken.startsWith("Bearer ")) refreshToken = refreshToken.substring(7);
             Authentication authentication = jwtTokenProvider.getRefreshAuthentication(refreshToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         // 헤더에 aceessToken을 보냈다면 accessToken 유효성 검사 및 authentication 세팅
         else if (token != null && jwtTokenProvider.validateAccessToken(token)) {
+            if (token.startsWith("Bearer ")) token = token.substring(7);
             Authentication authentication = jwtTokenProvider.getAccessAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
