6e5cf01cfefe132e2e068cae6969c218d99ba4bf
tjdgns8439 <tjdgns8439@inha.edu>
2024-03-20 14:11:23 +0900

    [MS-10] Refactor: S3Result에 fileName 추가

--- src/main/java/com/modutaxi/api/common/s3/dto/S3Result.java
+++ src/main/java/com/modutaxi/api/common/s3/dto/S3Result.java
@@ -1,9 +1,13 @@
 package com.modutaxi.api.common.s3.dto;
 
+import lombok.Getter;
+
+@Getter
 public class S3Result {
     private String imgUrl;
-
-    public S3Result(String imgUrl) {
+    private String fileName;
+    public S3Result(String imgUrl, String fileName) {
         this.imgUrl = imgUrl;
+        this.fileName = fileName;
     }
 }
