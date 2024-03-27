78b53d5448373bebd905ad187969cde05405d6d5
tjdgns8439 <tjdgns8439@inha.edu>
2024-03-27 20:13:16 +0900

    Revert "[MS-31] Feat: Naver derections5에 스펙에 따른 예외처리"
    This reverts commit 0390cd2fe5bde034224cf70eb212f79a120be71f.

--- src/main/java/com/modutaxi/api/domain/room/controller/GetRoomController.java
+++ src/main/java/com/modutaxi/api/domain/room/controller/GetRoomController.java
@@ -1,6 +1,12 @@
 package com.modutaxi.api.domain.room.controller;
 
+import com.google.gson.JsonObject;
+import com.modutaxi.api.domain.room.service.GetRoomService;
+import com.modutaxi.api.domain.room.service.GetTaxiInfoService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/rooms")
 public class GetRoomController {
 
+    private final GetRoomService getRoomService;
+    private final GetTaxiInfoService getTaxiInfoService;
+
+    @GetMapping("/driving")
+    public ResponseEntity<?> getDrivingInfo() {
+        return ResponseEntity.ok(getTaxiInfoService.getDrivingInfo());
+    }
 }
