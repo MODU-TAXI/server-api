664b4f247fdeb76ea0707ac9b39cc8af0524967f
이재원 <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-03-29 13:10:51 +0900

    [MS-43] Feat: 서버 시간대 서울로 변경 (#15)

--- src/main/java/com/modutaxi/api/ApiApplication.java
+++ src/main/java/com/modutaxi/api/ApiApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
