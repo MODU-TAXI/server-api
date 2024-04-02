f2201045e01fbdcbe4c2bdd6c9c48a51a1cd9700
tjdgns8439 <tjdgns8439@inha.edu>
2024-04-02 19:02:53 +0900

    [MS-30] Feat: room 관련 ErrorCode작성

---
+++ src/main/java/com/modutaxi/api/common/exception/errorcode/RoomErrorCode.java
@@ -0,0 +1,18 @@
+package com.modutaxi.api.common.exception.errorcode;
+
+import com.modutaxi.api.common.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum RoomErrorCode implements ErrorCode {
+    EMPTY_ROOM("ROOM_001", "존재하지 않는 방입니다.", HttpStatus.CONFLICT),
+    NOT_ROOM_MANAGER("ROOM_002", "권한이 없는 사용자입니다.", HttpStatus.CONFLICT),
+    ;
+
+    private final String errorCode;
+    private final String message;
+    private final HttpStatus status;
+}
