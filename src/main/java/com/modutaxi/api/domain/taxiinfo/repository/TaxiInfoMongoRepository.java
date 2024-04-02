272e8842f83f2cafb08805647d2f97f6b03051db
tjdgns8439 <tjdgns8439@inha.edu>
2024-04-03 01:22:21 +0900

    [MS-30] Refactor: TaxiInfoRepository명 수정

--- src/main/java/com/modutaxi/api/domain/taxiinfo/repository/TaxiInfoMongoRepository.java
+++ src/main/java/com/modutaxi/api/domain/taxiinfo/repository/TaxiInfoMongoRepository.java
@@ -3,5 +3,5 @@ package com.modutaxi.api.domain.taxiinfo.repository;
 import com.modutaxi.api.domain.taxiinfo.entity.TaxiInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TaxiInfoRepository extends MongoRepository<TaxiInfo, Long> {
+public interface TaxiInfoMongoRepository extends MongoRepository<TaxiInfo, Long> {
 }
