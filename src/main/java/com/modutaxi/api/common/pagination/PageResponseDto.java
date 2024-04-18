2b338bc6121706bdea1bb2f0663dcb82f55b5975
tjdgns8439 <tjdgns8439@inha.edu>
2024-04-18 22:29:45 +0900

    [MS-80] Feat: 페이징처리

---
+++ src/main/java/com/modutaxi/api/common/pagination/PageResponseDto.java
@@ -0,0 +1,12 @@
+package com.modutaxi.api.common.pagination;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PageResponseDto<T> {
+    private int page;
+    private boolean hasNext;
+    private T result;
+}
