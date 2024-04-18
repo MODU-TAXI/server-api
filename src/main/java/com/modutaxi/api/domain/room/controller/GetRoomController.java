22e7672d2c6e216bb399cb9c73a56efbe5408dfa
tjdgns8439 <tjdgns8439@inha.edu>
2024-04-18 22:29:36 +0900

    [MS-80] Feat: 존재하는 방 리스트 조회 구현

--- src/main/java/com/modutaxi/api/domain/room/controller/GetRoomController.java
+++ src/main/java/com/modutaxi/api/domain/room/controller/GetRoomController.java
@@ -1,14 +1,19 @@
 package com.modutaxi.api.domain.room.controller;
 
+import com.modutaxi.api.common.pagination.PageResponseDto;
 import com.modutaxi.api.domain.room.dto.RoomResponseDto.RoomDetailResponse;
+import com.modutaxi.api.domain.room.dto.RoomResponseDto.RoomSimpleResponse;
 import com.modutaxi.api.domain.room.service.GetRoomService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +31,13 @@ public class GetRoomController {
     @GetMapping("/{id}")
     public ResponseEntity<RoomDetailResponse> getRoomDetail(@PathVariable Long id) {
         return ResponseEntity.ok(getRoomService.getRoomDetail(id));
+    }
+
+    @Operation(summary = "경로를 제외한 방 리스트 조회",
+        description = "\n\n\n**page(0 ~ )** : 페이지 번호\n\n**size(1 ~ )** : 사이즈")
+    @GetMapping
+    public ResponseEntity<PageResponseDto<List<RoomSimpleResponse>>> getRoomSimpleList(
+        @RequestParam int page, @RequestParam int size) {
+        return ResponseEntity.ok(getRoomService.getRoomSimpleList(page, size));
     }
 }
--- src/main/java/com/modutaxi/api/domain/room/repository/RoomRepository.java
+++ src/main/java/com/modutaxi/api/domain/room/repository/RoomRepository.java
@@ -1,8 +1,12 @@
 package com.modutaxi.api.domain.room.repository;
 
 import com.modutaxi.api.domain.room.entity.Room;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     boolean existsRoomByRoomManagerId(Long memberId);
+    Slice<Room> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
