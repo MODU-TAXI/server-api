17edf826c5a856c97b4b53f2e5dd9ff6e1559f85
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-04-19 03:51:56 +0900

    [MS-84] Feat: 즐겨찾기 생성

--- src/main/java/com/modutaxi/api/domain/likedSpot/dto/LikedSpotResponseDto.java
+++ src/main/java/com/modutaxi/api/domain/likedSpot/dto/LikedSpotResponseDto.java
@@ -1,5 +1,14 @@
 package com.modutaxi.api.domain.likedSpot.dto;
 
-public class LikedSpotResponseDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+public class LikedSpotResponseDto {
+    @Getter
+    @AllArgsConstructor
+    public static class LikedSpotResponse {
+        @Schema(example = "true", description = "수행 여부")
+        private Boolean isOperated;
+    }
 }
--- src/main/java/com/modutaxi/api/domain/likedSpot/repository/LikedSpotRepository.java
+++ src/main/java/com/modutaxi/api/domain/likedSpot/repository/LikedSpotRepository.java
@@ -1,8 +1,9 @@
 package com.modutaxi.api.domain.likedSpot.repository;
 
 import com.modutaxi.api.domain.likedSpot.entity.LikedSpot;
+import com.modutaxi.api.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikedSpotRepository extends JpaRepository<LikedSpot, Long> {
-
+    Boolean existsByMemberAndSpotId(Member member, Long spotId);
 }
