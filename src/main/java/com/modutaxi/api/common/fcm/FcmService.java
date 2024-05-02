26c2bcb3c1c329cf82cbd3bc1ac8f8c33d566ea0
bora <youonl5@naver.com>
2024-05-02 20:22:04 +0900

    [MS-119] Feat: 메세지 발송 메서드 구현

--- src/main/java/com/modutaxi/api/common/fcm/FcmService.java
+++ src/main/java/com/modutaxi/api/common/fcm/FcmService.java
@@ -2,6 +2,8 @@ package com.modutaxi.api.common.fcm;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.gson.Gson;
 import com.modutaxi.api.common.exception.BaseException;
 import com.modutaxi.api.common.exception.errorcode.ChatErrorCode;
 import com.modutaxi.api.domain.member.entity.Member;
@@ -16,6 +18,7 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class FcmService {
 
+    private final FirebaseMessaging firebaseMessaging;
     private final RedisFcmRepositoryImpl redisFcmRepository;
 
     public void subscribe(Member member, Long chatroomId) {
@@ -31,4 +34,78 @@ public class FcmService {
         }
     }
 
+    public void send(Message message) {
+        try {
+            FirebaseMessaging.getInstance().send(message);
+            Gson gson = new Gson();
+            String fcmMessageJson = gson.toJson(message);
+            log.info("FCM 메시지: " + fcmMessageJson);
+        } catch (FirebaseMessagingException e) {
+            throw new BaseException(ChatErrorCode.FAIL_SEND_MESSAGE);
+        }
+    }
+
+    /**
+     * 채팅 알림
+     * TODO: 파라미터는 적절히 바꿔주세요.
+     */
+    public void sendChatMessage(Member member,
+                                Long chatroomId, String nickname, String content, String createAt) {
+        String fcmToken = redisFcmRepository.findById(member.getId());
+        Message message = Message.builder()
+                .putData("type", "MESSAGE")
+                .putData("chatroomId", Long.toString(chatroomId))
+                .putData("message", content)
+                .putData("nickName", nickname)
+                .putData("createAt", createAt)
+                .setToken(fcmToken)
+                .build();
+        send(message);
+    }
+
+    /**
+     * 새로운 참여자 알림
+     * TODO: 파라미터는 적절히 바꿔주세요.
+     */
+    public void sendNewParticipant(Member member, Long chatroomId, String createAt) {
+        String fcmToken = redisFcmRepository.findById(member.getId());
+        Message message = Message.builder()
+                .putData("type", "NEW_PARTICIPANT")
+                .putData("chatroomId", Long.toString(chatroomId))
+                .putData("createAt", createAt)
+                .putData("message", "새로운 참여자 입장 키야아아!!")
+                .setToken(fcmToken)
+                .build();
+        send(message);
+    }
+
+    /**
+     * 매칭 완료 알림
+     * TODO: 파라미터는 적절히 바꿔주세요.
+     */
+    public void sendSuccessMatching(Member member, Long chatroomId) {
+        String fcmToken = redisFcmRepository.findById(member.getId());
+        Message message = Message.builder()
+                .putData("chatroomId", Long.toString(chatroomId))
+                .putData("type", "SUCCESS_MATCHING")
+                .putData("message", "어디어디 방 매칭에 성공했어요.")
+                .setToken(fcmToken)
+                .build();
+        send(message);
+    }
+
+    /**
+     * (방장) 새로운 사용자 매칭 등록 알림
+     * TODO: 파라미터는 적절히 바꿔주세요.
+     */
+    public void sendNewMatching(Member member, Long chatroomId) {
+        String fcmToken = redisFcmRepository.findById(member.getId());
+        Message message = Message.builder()
+                .putData("chatroomId", Long.toString(chatroomId))
+                .putData("type", "NEW_MATCHING")
+                .putData("message", "어디어디 방 매칭에 성공했어요.")
+                .setToken(fcmToken)
+                .build();
+        send(message);
+    }
 }
