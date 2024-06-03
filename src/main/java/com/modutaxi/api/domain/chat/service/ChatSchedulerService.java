cfcb689eb18cc1626f2b5cbe8da49c299b510c06
tjdgns8439 <tjdgns8439@inha.edu>
2024-06-04 00:35:19 +0900

    [MS-149] Feat: 동적 예약 알림 기능 구현
    - 방 생성 시 스케줄러에 Job 추가

---
+++ src/main/java/com/modutaxi/api/domain/chat/service/ChatSchedulerService.java
@@ -0,0 +1,75 @@
+package com.modutaxi.api.domain.chat.service;
+
+import com.modutaxi.api.common.exception.BaseException;
+import com.modutaxi.api.common.exception.errorcode.RoomErrorCode;
+import com.modutaxi.api.domain.chatmessage.dto.ChatMessageRequestDto;
+import com.modutaxi.api.domain.chatmessage.entity.MessageType;
+import com.modutaxi.api.domain.chatmessage.mapper.ChatMessageMapper;
+import com.modutaxi.api.domain.chatmessage.repository.ChatMessageRepository;
+import com.modutaxi.api.domain.room.entity.Room;
+import com.modutaxi.api.domain.room.repository.RoomRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Slf4j
+@Service
+public class ChatSchedulerService {
+
+    private final TaskScheduler taskScheduler;
+    private final ChatService chatService;
+
+    // TODO: 6/4/24 전체적으로 메세지 저장로직을 전송 로직과 묶기
+    private final ChatMessageRepository chatMessageRepository;
+    private final RoomRepository roomRepository;
+
+    private static final long FIVE_MINUTES = 300;
+
+    private Runnable matchingModal(Long roomId) {
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> new BaseException(RoomErrorCode.EMPTY_ROOM));
+        return () -> {
+            ChatMessageRequestDto message = new ChatMessageRequestDto(
+                    Long.valueOf(roomId), MessageType.CHAT_BOT, "매칭완료 하시겠습니까?",
+                    "모두의택시", room.getRoomManager().getId().toString(), LocalDateTime.now());
+            chatService.sendChatMessage(message);
+            chatMessageRepository.save(ChatMessageMapper.toEntity(message, room));
+            log.info("매칭완료 알림이 실행되었습니다: {}", Thread.currentThread().getName());
+        };
+    }
+
+    private Runnable callTaxi(Long roomId) {
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> new BaseException(RoomErrorCode.EMPTY_ROOM));
+        return () -> {
+            ChatMessageRequestDto message = new ChatMessageRequestDto(
+                    Long.valueOf(roomId), MessageType.CHAT_BOT, "택시 부르러 가볼까요?",
+                    "모두의택시", room.getRoomManager().getId().toString(), LocalDateTime.now());
+            chatService.sendChatMessage(message);
+            chatMessageRepository.save(ChatMessageMapper.toEntity(message, room));
+            log.info("5분전 알림이 실행되었습니다: {}", Thread.currentThread().getName());
+        };
+    }
+
+
+    public void addTask(Long roomId, LocalDateTime departureTime) {
+        //매칭완료 알림 전송
+        long delaySeconds = calculateDelaySeconds(LocalDateTime.now(), departureTime);
+
+        Runnable matchingModal = matchingModal(roomId);
+        taskScheduler.schedule(matchingModal, Instant.now().plusSeconds(delaySeconds));
+
+        Runnable callTaxiTask = callTaxi(roomId);
+        delaySeconds = delaySeconds > FIVE_MINUTES + 20 ? delaySeconds - FIVE_MINUTES : 0;
+        taskScheduler.schedule(callTaxiTask, Instant.now().plusSeconds(delaySeconds));
+    }
+
+    private long calculateDelaySeconds(LocalDateTime now, LocalDateTime targetDateTime) {
+        Duration duration = Duration.between(now, targetDateTime);
+        return Math.max(0, duration.getSeconds());
+    }
+}
