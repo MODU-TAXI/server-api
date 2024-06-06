04cf1054cc4e1b6f717d67d2e59f61d9116eaa5b
tjdgns8439 <tjdgns8439@inha.edu>
2024-06-07 00:44:19 +0900

    Test: Access Time 수정 30초 -> 24시간

--- src/main/java/com/modutaxi/api/common/auth/jwt/JwtTokenProvider.java
+++ src/main/java/com/modutaxi/api/common/auth/jwt/JwtTokenProvider.java
@@ -33,7 +33,7 @@ public class JwtTokenProvider {
 
     private static final String AUTHORIZATION_HEADER = "Authorization"; // 액세스 토큰 헤더 key name
     private static final String REFRESH_HEADER = "refreshToken";  // 리프레시 토큰 헤더 key name
-    private static final long TOKEN_VALID_TIME = 1000 * 30L;  // 유효기간 1시간
+    private static final long TOKEN_VALID_TIME = 1000 * 24 * 60 * 60L;  // 유효기간 1시간
     private static final long REF_TOKEN_VALID_TIME = 1000 * 60L * 60L * 24L * 7L;  // 유효기간 일주일
 
     private final PrincipalDetailsService principalDetailsService;
