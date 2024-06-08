9772252df9ea4439b64bdaea9fd98748a0f403fa
tjdgns8439 <tjdgns8439@inha.edu>
2024-06-08 18:15:35 +0900

    [MS-151] Chore: StompErrorCode 주석 추가

--- src/main/java/com/modutaxi/api/common/exception/errorcode/StompErrorCode.java
+++ src/main/java/com/modutaxi/api/common/exception/errorcode/StompErrorCode.java
@@ -9,14 +9,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum StompErrorCode implements ErrorCode {
+    //방 관련
     FULL_CHAT_ROOM("SOCK_ROOM_001", "방이 꽉 차있습니다.", HttpStatus.BAD_REQUEST),
     ROOM_ID_IS_NULL("SOCK_ROOM_002", "방 ID 가 null입니다.", HttpStatus.BAD_REQUEST),
     FAULT_ROOM_ID("SOCK_ROOM_003", "잘못된 방 번호입니다.", HttpStatus.BAD_REQUEST),
     ALREADY_ROOM_IN("SOCK_ROOM_004", "당신은 이미 방에 참여한 상태입니다.", HttpStatus.BAD_REQUEST),
+
+
+    //pub/sub 관련
     FAIL_SEND_MESSAGE("SOCK_MESSAGE_005","메세지 전송에 실패하였습니다.", HttpStatus.CONFLICT),
 
+    //멤버 관련
     EMPTY_MEMBER("SOCK_MEMBER_006","존재하지 않는 멤버입니다.", HttpStatus.BAD_REQUEST),
 
+    //login 관련
     EMPTY_JWT("SOCK_AUTH_007", "JWT가 없습니다.", HttpStatus.UNAUTHORIZED),
     INVALID_JWT("SOCK_AUTH_008", "유효하지 않은 JWT입니다.", HttpStatus.UNAUTHORIZED),
     EXPIRED_MEMBER_JWT("SOCK_AUTH_009", "만료된 JWT입니다.", HttpStatus.GONE),
