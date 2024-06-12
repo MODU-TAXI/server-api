194c5954a21a8393e41a4e4216ddbf4af70adbe4
bora <youonl5@naver.com>
2024-06-13 00:37:33 +0900

    [MS-130] Refactor: 정산 요청 API HTTP Method 변경

--- src/main/java/com/modutaxi/api/domain/paymentmember/controller/UpdatePaymentMemberController.java
+++ src/main/java/com/modutaxi/api/domain/paymentmember/controller/UpdatePaymentMemberController.java
@@ -15,7 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,7 +72,7 @@ public class UpdatePaymentMemberController {
                 """),
         })),
     })
-    @GetMapping("")
+    @PatchMapping("")
     public ResponseEntity<UpdatePaymentMemberResponse> updatePaymentMembersToComplete(
         @CurrentMember Member member,
         @Parameter(description = "택시팟의 id") @RequestParam("roomId") Long roomId
