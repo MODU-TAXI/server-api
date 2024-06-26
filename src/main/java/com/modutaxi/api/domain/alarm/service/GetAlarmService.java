ee5e19021367e59a00419bf8850b763ace37b94b
bora <youonl5@naver.com>
2024-06-26 19:58:31 +0900

    [MS-145] Feat: 알림 조회 API 읽음 처리 로직 추가

--- src/main/java/com/modutaxi/api/domain/alarm/service/GetAlarmService.java
+++ src/main/java/com/modutaxi/api/domain/alarm/service/GetAlarmService.java
@@ -1,11 +1,14 @@
 package com.modutaxi.api.domain.alarm.service;
 
+import static com.modutaxi.api.domain.alarm.mapper.AlarmMapper.toDto;
+
 import com.modutaxi.api.common.pagination.PageResponseDto;
 import com.modutaxi.api.domain.alarm.dto.AlarmResponseDto.AlarmInfo;
 import com.modutaxi.api.domain.alarm.entity.Alarm;
-import com.modutaxi.api.domain.alarm.mapper.AlarmMapper;
 import com.modutaxi.api.domain.alarm.repository.AlarmRepository;
 import com.modutaxi.api.domain.member.entity.Member;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,11 +22,18 @@ public class GetAlarmService {
 
     private final AlarmRepository alarmRepository;
 
+    @Transactional
     public PageResponseDto<List<AlarmInfo>> getAlarmList(Member member, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Alarm> alarms = alarmRepository.findByMemberIdOrderByCreatedAtDesc(member.getId(),
             pageable);
-        List<AlarmInfo> alarmInfos = alarms.stream().map(AlarmMapper::toDto).toList();
+
+        List<AlarmInfo> alarmInfos = new ArrayList<>();
+        alarms.forEach(alarm -> {
+                alarmInfos.add(toDto(alarm));
+                alarm.setCheckedTrue();     // 모든 알림에 대해 읽음 처리
+            }
+        );
         return new PageResponseDto<>(page, alarms.hasNext(), alarmInfos);
     }
 }
