d83d30a5de36a4aa9df4e6e1be0d19526e856985
tjdgns8439 <tjdgns8439@inha.edu>
2024-07-30 16:52:15 +0900

    [MS-192] Refactor: FCM 상위 클래스 예외로 통합하여 예외 처리 단순화

--- src/main/java/com/modutaxi/api/common/fcm/FcmService.java
+++ src/main/java/com/modutaxi/api/common/fcm/FcmService.java
@@ -2,10 +2,10 @@ package com.modutaxi.api.common.fcm;
 
 import static com.modutaxi.api.common.constants.ServerConstants.FCM_DEFAULT_TOKEN;
 
+import com.google.firebase.FirebaseException;
 import com.google.firebase.messaging.ApnsConfig;
 import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import com.google.gson.Gson;
@@ -51,7 +51,7 @@ public class FcmService {
                 .subscribeToTopic(
                     Collections.singletonList(fcmToken), Long.toString(roomId));
             log.info("FCM SUBSCRIBE");
-        } catch (FirebaseMessagingException e) {
+        } catch (FirebaseException e) {
             log.error("FAIL FCM SUBSCRIBE");
             throw new BaseException(ChatErrorCode.FAIL_FCM_SUBSCRIBE);
         }
@@ -63,7 +63,7 @@ public class FcmService {
             FirebaseMessaging.getInstance()
                 .unsubscribeFromTopic(
                     Collections.singletonList(fcmToken), Long.toString(roomId));
-        } catch (FirebaseMessagingException e) {
+        } catch (FirebaseException e) {
             throw new BaseException(ChatErrorCode.FAIL_FCM_UNSUBSCRIBE);
         }
     }
@@ -74,7 +74,7 @@ public class FcmService {
             Gson gson = new Gson();
             String fcmMessageJson = gson.toJson(message);
             log.info("FCM 메시지: " + fcmMessageJson);
-        } catch (FirebaseMessagingException e) {
+        } catch (FirebaseException e) {
             log.error(ChatErrorCode.FAIL_SEND_MESSAGE.getMessage());
 //            throw new BaseException(ChatErrorCode.FAIL_SEND_MESSAGE);
         }
